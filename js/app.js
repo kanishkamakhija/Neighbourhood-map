05752f4d2f1f9c5ce7a86a4ac0aee514185cfffa
Kanishka <kanishkamakhija007@gmail.com>
2017-08-15 04:55:52 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,141 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+
+function mysrch() {
+    var self = this;
+    this.searchPlace = ko.observable('');
+    this.searchPlace = document.getElementById('places-search').value;
+    var ul, li;
+    ul = document.getElementById("myUL");
+    p = ul.getElementsByTagName('p');
+    if (this.searchPlace != null) {
+        for (i = 0; i < p.length; i++) {
+            var res = p[i].innerHTML.toLowerCase();
+            if (res.indexOf(this.searchPlace.toLowerCase()) >= 0) {
+                p[i].style.display = "";
+            } else {
+                p[i].style.display = "none";
+            }
+        }
+    } else {
+        ul.style.display = "";
+    }
+}
+
+var bounds;
+var viewModel = function() {
+    "use strict";
+
+    var self = this;
+    var markers = [];
+    var marker;
+    var content;
+
+    self.searchPlace = ko.observable('');
+
+    self.mrkers = function() {
+        if (this.searchPlace().length == '') {
+            this.showAll();
+        } else {
+            for (var i = 0; i < locations.length; i++) {
+                if (locations[i].title.indexOf(this.searchPlace().toLowerCase()) >= 0) {
+                    markers[i].setMap(map);
+                } else {
+                    markers[i].setMap(null);
+                }
+            }
+        }
+    }
+
+    self.infowindow = new google.maps.InfoWindow({
+        maxWidth: 300,
+        content: ""
+    });
+
+    self.populateinfowindow = function(marker, infowindow) {
+        // check to mk sure infowindow is not alrdy closed
+        if (self.infowindow.marker != marker) {
+            self.infowindow.marker = marker;
+            content = '<div>' + marker.title + '<br>' + marker.likes + '<br>' + marker.rating + '<br><img src="' + marker.img + '" alt="marker-img" height="100" width="100"/>' + '</div>'
+            self.infowindow.setContent(content);
+            self.infowindow.open(map, marker);
+            // self.infowindow.addListener('closeclick', function() {
+            //     self.infowindow.setMarker(null);
+            // });
+            // map.panTo(marker.getPosition())
+
+        }
+    };
+
+    self.toggleBounce = function(marker) {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function() {
+            marker.setAnimation(null);
+        }, 500);
+    };
+
+    self.addInfoToWindow = function(marker) {
+        $.ajax({
+            url: "https://api.foursquare.com/v2/venues/" + marker.id + '?client_id=4KZBB34R2W4APRMIJIJX1DWUO04NP2PNCQJX2EFK5PZLV1CD&client_secret=CPT2SIUZAV5WTUNTVGG0WI3ZAJKO4I0HJ5TV2TE3U3I3KW5R&v=20170208',
+            dataType: "json",
+            success: function(data) {
+                // stores results to display likes and ratings
+                var result = data.response.venue;
+                // add likes and ratings to marker
+                marker.likes = result.hasOwnProperty('likes') ? result.likes.summary : "";
+                marker.rating = result.hasOwnProperty('rating') ? result.rating : "";
+            },
+            //alert if there is error in recievng json
+            error: function(xhr, status, thrownError) {
+                console.log("Foursquare data is unavailable. Please try again later.");
+            }
+        });
+    };
+    //this.addListener = google.maps.event.addListener(this.map, 'click', (this.infowindow.close(this.infowindow)));
+    self.bounds = new google.maps.LatLngBounds();
+    for (var i = 0; i < locations.length; i++) {
+        marker = new google.maps.Marker({
+            position: locations[i].location,
+            title: locations[i].title,
+            animation: google.maps.Animation.DROP,
+            id: locations[i].id,
+            img: locations[i].icon,
+
+        });
+        self.addInfoToWindow(marker);
+        markers.push(marker);
+        marker.addListener('click', function() {
+            self.toggleBounce(this);
+        });
+        marker.addListener('click', function() {
+            self.populateinfowindow(this, self.infowindow);
+        });
+
+    }
+
+    self.showAll = function() {
+        for (var i = 0; i < locations.length; i++) {
+            markers[i].setMap(map);
+        }
+    };
+    self.showAll();
+}
---
+++ js/map-data.js
@@ -0,0 +1,52 @@
+var locations = [{
+        title: 'red fort',
+        location: {
+            lat: 28.6562,
+            lng: 77.2410
+        },
+        icon: "img/redfort.jpg",
+        id: '55cea20f498e9b5be13e520d'
+    },
+
+    {
+        title: 'qutb minar',
+        location: {
+            lat: 28.5244,
+            lng: 77.1855
+        },
+        icon: "img/qutbminar.jpg",
+        id: '4ba47c9bf964a520fe9f38e3'
+    },
+
+    {
+        title: 'jama masjid',
+        location: {
+            lat: 28.6507,
+            lng: 77.2334
+        },
+        icon: "img/jamamasjid.jpg",
+        id: '531517c2e4b0e72a36f1d682'
+    },
+
+    {
+        title: "humayun's tomb",
+        location: {
+            lat: 28.5933,
+            lng: 77.2507
+        },
+        icon: "img/humayunstomb.jpg",
+        id: '564c86ca498e94b8cec8db46'
+    },
+
+    {
+        title: "akshardham",
+        location: {
+            lat: 28.6127,
+            lng: 77.2773
+        },
+        icon : "img/akshardham.jpg",
+        id: '53490086498e429cc0013382'
+
+    }
+
+];
